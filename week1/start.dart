4982934d227badba91c029bcc440df8b317ae58d
Hakan Tekin <hakantekin2002@gmail.com>
2025-12-03 18:35:42 +0300

    remove old files, keep only week3

--- week1/start.dart
+++
@@ -1,5 +0,0 @@
-void main (){
-  double user_money = 100 ;
-  user_money /= 2;
-  print('ahmet beyin parası: $user_money');
-}
--- week1/user_advance.dart
+++
@@ -1,6 +0,0 @@
-void main (){;
-const int bankCustomer1Money = 100;
-  int bankCustomer2Money = 29;
-
-  print(bankCustomer1Money - bankCustomer2Money);
-}
